b659869fcf17793a55d136f8631573d3ffd32d1a
PiyushUpadhyaya <upadhyaya.piyu@gmail.com>
2017-05-11 01:22:11 +0530

    Text 2 png working
    The get quote url fetches a random code and text2png module saves it as
    a .png extension file.
    Example: upload.png

--- server.js
+++ server.js
@@ -7,19 +7,27 @@ connect.createServer
 */
 const express = require('express');
 const bodyParser= require('body-parser');
-const fs = require('fs');
 var validator = require('express-validator');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
-
+var https = require('https');
 var mongojs = require('mongojs');
 var db = mongojs('quotes', ['quotes']);
-var FB = require('fb');
-
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(validator());
+
+
+var Jimp  = require('jimp');
+var fs = require('fs');
+var text2png = require('text2png');
+
+var FormData = require('form-data');
+var FB = require('fb');
+var request = require('request');
+var access_token = "EAACEdEose0cBAN8nowi0in5wllGV7qZBsEoqCAYZAPyDRbSo8e9ixGccGLitgEsgSWB0IIhmpIXDybQaZA96TxxoZCligZBZACxsC4Ejo9AD3PdAEgzlhGs5ZB7u61UK1CZAk40natLZC3qUP68ZBLcK8h5qG4t4wHAEVBiUZAUXuBKg4RnMC9mKEQmC2BszxMZBw6pZBqCt95EvQ6EhXwZB0FrsAGVZCI6qn6z5vgZD";
+
 
            ///For index.html*/
     app.post('/post_quote', (req, res) =>{
@@ -40,35 +48,72 @@ app.use(validator());
     app.post('/get_quote', (req, res) => {
         console.log("Getting Quote of Genre :::");
         console.log(req.body.genre);
-        //res.render('company_unregistered.ejs');  
          db.quotes.find().toArray((err, result) => {
          if (err) return console.log(err)
-        // renders index.ejs
          res.render('get_quotes.ejs', {output: result})
+         fs.writeFileSync('upload.png',text2png(output,{textColor: 'blue'}));
         });
     });
 
-app.post('/post_fb', (req, res) => {
-        FB.setAccessToken('access_token');
- 
-        FB.api('me/photos', 'post', { source: fs.createReadStream('my-vacation.jpg'), caption: 'My vacation' }, function (res) {
-          if(!res || res.error) {
-            console.log(!res ? 'error occurred' : res.error);
-            return;
-          }
-          console.log('Post Id: ' + res.post_id);
+///post on facebook
+    app.post('/post_fb', (req,res) => {
+         var form = new FormData();
+        var options = {
+            method: 'post',
+            host: 'www.facebook.com',
+            path: '/me/photos?access_token='+access_token,
+            headers: form.getHeaders(),
+        }
+         var request = https.request(options,function (res) {
+            console.log("Looks like something is going fine");
+          //  console.log(res);
         });
+        
+        request.post(
+            {
+                url: 'https://graph.facebook.com/me/photos?access_token=' + access_token, 
+                formData: {
+                    message: 'message',
+                    source: fs.createReadStream('/images/test.jpg') 
+                }
+            }, function(err, res, body) {
+                var bodyJSON = JSON.parse(body);
+                if(bodyJSON.error) {
+                    console.log(bodyJSON.error.message);
+                }
+            }
+        );
+       /* FB.getLoginStatus(function(response) {
+          if (response.status === 'connected') {
+            var access_token = response.authResponse.accessToken;
+              console.log(access_token)
+          } 
+        });
+        console.log("initial stage of posting data to FB");
+        console.log(req.body);
+            
+        var form = new FormData();
+        form.append('file',fs.createReadStream(__dirname+'/test.jpg'));
+        form.append('message',"Gaitoo");
 
-       /* FB.api('me/photos', 'post', { source: { value: photoBuffer, options: { contentType: 'image/jpeg' } }, caption: 'My vacation' }, function (res) {
-          if(!res || res.error) {
-            console.log(!res ? 'error occurred' : res.error);
-            return;
-          }
-          console.log('Post Id: ' + res.post_id);
+        var options = {
+            method: 'post',
+            host: 'www.facebook.com',
+            path: '/me/photos?access_token='+access_token,
+            headers: form.getHeaders(),
+        }
+        var request = https.request(options,function (res) {
+            console.log("Looks like something is going fine");
+          //  console.log(res);
+        });
+        console.log("makng form pipe")
+        form.pipe(request);
+        console.log("we are here out of request");
+        request.on('error', function (error) {
+            console.log("requestwise error on facebook")
+            console.log(error);
         });*/
-    
-});
-
-
+        
+    })
 app.listen(3000);
 console.log("Server running on port 3000")
